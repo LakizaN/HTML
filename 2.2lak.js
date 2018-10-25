f8f83a8c2519f220a2a7e8b2b2ab16f938afe2a3
LakizaN <39501346+LakizaN@users.noreply.github.com>
2018-10-25 12:25:27 +0300

    Add files via upload

---
+++ 2.2lak.js
@@ -0,0 +1,30 @@
+﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+
+
+function f1()
+{
+if($('sl21').selectedIndex==0)
+$("p").src="чирва.png";
+if($('sl21').selectedIndex==1)
+$('p').src="трефа.png";
+if($('sl21').selectedIndex==2)
+$('p').src="пика.png";
+if($('sl21').selectedIndex==3)
+$('p').src="бубна.png";
+}
+
+function rr()
+{
+$ ('areal').value="ПРИВЕТ, МИР!!!";
+}
+
