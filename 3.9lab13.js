44be9feb1b0363ab0498dadbbd5dc96003f8500e
LakizaN <39501346+LakizaN@users.noreply.github.com>
2018-12-13 12:20:46 +0200

    Update 3.9lab13.js

--- 3.9lab13.js
+++ 3.9lab13.js
@@ -6,7 +6,7 @@ var r1=500;
 function f()
 {
 var x1=document.getElementById('b11');
-x1.style.top=(parseInt(x1.style.top)-90)+'px';
+x1.style.top=(parseInt(x1.style.top)-80)+'px';
 x1.style.left=(parseInt(x1.style.left))+'px';
 b1-=150;
 g1-=1000;
@@ -15,7 +15,7 @@ str1='rgb('+b1+', '+g1+', '+r1+')';
 x1.style.backgroundColor="white";
 }
 function k()
-{t=setInterval('f()',500);}
+{t=setInterval('f()',100);}
 function g()
 {
 clearInterval(t);
