cae7c3934dd43a609a10604c1f82f21ad232beec
LakizaN <39501346+LakizaN@users.noreply.github.com>
2018-12-13 11:57:53 +0200

    Add files via upload

---
+++ 3.7lab9.js
@@ -0,0 +1,24 @@
+﻿
+function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+
+
+function f1()
+{N=parseFloat($('tt1').value);
+                       K=0;
+                while(Math.pow(3,K) < N)
+		       K++;
+                       K--;
+                      $('tt2').value=K;
+                             
+
+ }
