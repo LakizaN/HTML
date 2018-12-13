0b8a5410fbe92f0680ce1bc0a34516efa5ab0361
LakizaN <39501346+LakizaN@users.noreply.github.com>
2018-12-13 11:22:14 +0200

    Add files via upload

---
+++ 3.9lab13.js
@@ -0,0 +1,22 @@
+﻿
+var t;
+var b1=110;
+var g1=100;
+var r1=500;
+function f()
+{
+var x1=document.getElementById('b11');
+x1.style.top=(parseInt(x1.style.top)-90)+'px';
+x1.style.left=(parseInt(x1.style.left))+'px';
+b1-=150;
+g1-=1000;
+r1-=80;
+str1='rgb('+b1+', '+g1+', '+r1+')';
+x1.style.backgroundColor="white";
+}
+function k()
+{t=setInterval('f()',500);}
+function g()
+{
+clearInterval(t);
+}
